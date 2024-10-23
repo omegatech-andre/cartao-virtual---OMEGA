c11e182950e7cfffd3a2105e9c11c7bdf1602888
omegatech-andre <omega.ti_ind@outlook.com>
2024-10-23 09:56:41 -0300

    feat: desativando botao do silk xtreme

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ export default function Page() {
     setIsVisible(!isVisible)
     setIsRotate(!isRotate)
   }
-  
+
   return (
     <main>
       <div className="page">
@@ -31,23 +31,23 @@ export default function Page() {
           isVisible && (
             <>
               <div className="page__content">
-                <ButtonAtv link="https://chat.whatsapp.com/JeKkKdpVGbNBMr5ddPwVQ9" target="_blank" largura="15rem" nome="Silk xTreme" />
+                <ButtonAtv link="" target="_blank" largura="15rem" nome="Silk xTreme" />
                 <ButtonAtv link="https://www.omegascreen.com.br/" target="_blank" largura="15rem" nome="Site Oficial" />
                 <ButtonAtv link="https://www.canva.com/design/DAFQKSiFu9E/mkECBY5Sy9WzJY-5Hbx6Xw/view?utm_content=DAFQKSiFu9E&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank" largura="15rem" nome="Portfólio" />
                 <ButtonAtv link="https://api.whatsapp.com/send/?phone=5581982810058&text&type=phone_number&app_absent=0" target="_blank" largura="15rem" nome="Atendimento" />
               </div>
               <div className="page__content">
-                <a href="https://www.tiktok.com/@omegascreen.ind?is_from_webapp=1&sender_device=pc" target='_blank'><SiTiktok size={22}/></a>
-                <a href="https://www.youtube.com/@omegascreen" target='_blank'><SiYoutube size={22}/></a>
-                <a href="https://www.linkedin.com/company/%C3%B4mega-ind%C3%BAstria-e-com%C3%A9rcio-de-tintas-ltda/?originalSubdomain=br" target='_blank'><SiLinkedin size={22}/></a>
-                <a href="https://www.facebook.com/profile.php?id=100086183216972" target='_blank'><SiFacebook size={22}/></a>
-                <a href="https://www.instagram.com/omegascreen.ind" target='_blank'><SiInstagram size={22}/></a>
+                <a href="https://www.tiktok.com/@omegascreen.ind?is_from_webapp=1&sender_device=pc" target='_blank'><SiTiktok size={22} /></a>
+                <a href="https://www.youtube.com/@omegascreen" target='_blank'><SiYoutube size={22} /></a>
+                <a href="https://www.linkedin.com/company/%C3%B4mega-ind%C3%BAstria-e-com%C3%A9rcio-de-tintas-ltda/?originalSubdomain=br" target='_blank'><SiLinkedin size={22} /></a>
+                <a href="https://www.facebook.com/profile.php?id=100086183216972" target='_blank'><SiFacebook size={22} /></a>
+                <a href="https://www.instagram.com/omegascreen.ind" target='_blank'><SiInstagram size={22} /></a>
               </div>
             </>
           )
         }
         <div className="page__btntoggle">
-          <IoMenu size={30} onClick={handleClick} className={isRotate ? 'rotateX' : 'rotateY'}/>
+          <IoMenu size={30} onClick={handleClick} className={isRotate ? 'rotateX' : 'rotateY'} />
         </div>
       </div>
     </main >
